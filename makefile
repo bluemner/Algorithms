27fd3cc9a954132d292ad979947cca5611ae272a
brandon Bluemner <brandonbluemner@gmail.com>
2016-03-11 23:03:43 -0600

    Added max sum

--- makefile
+++ makefile
@@ -2,13 +2,16 @@ SOURCE_FILES = main.cpp
 COMPILER = g++
 FLAGS =-w -ggdb
 OUTPUT_FILE = bin/run.o
-all: a thread beta dijkstra
+OUTPUT_DIR = bin/
+all: a thread beta dijkstra maxSum
 	
 a: $(SOURCE_FILES)
 	$(COMPILER) $(FLAGS) $(SOURCE_FILES) -o $(OUTPUT_FILE)
 thread: test1.cpp
-	$(COMPILER) $(FLAGS) -std=c++11 -pthread test1.cpp -o bin/test1.o
+	$(COMPILER) $(FLAGS) -std=c++11 -pthread test1.cpp -o $(OUTPUT_DIR)test1.o
 beta: source/beta.cpp headers/beta.h
-	 $(COMPILER) $(FLAGS) -std=c++11 source/beta.cpp -o bin/beta.o
+	$(COMPILER) $(FLAGS) -std=c++11 source/beta.cpp -o $(OUTPUT_DIR)beta.o
 dijkstra: source/dijkstra.cpp 
-	 $(COMPILER) $(FLAGS) -std=c++11 source/dijkstra.cpp -o bin/dijkstra.o
+	$(COMPILER) $(FLAGS) -std=c++11 source/dijkstra.cpp -o $(OUTPUT_DIR)dijkstra.o
+maxSum: source/maxSum.cpp 
+	$(COMPILER) $(FLAGS) -std=c++11 source/maxSum.cpp -o $(OUTPUT_DIR)maxSum.o
---
+++ source/maxSum.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <limits>
+
+namespace betacore
+{
+	void kadane(int input[], int n, int &x1, int &x2, int &max)  
+	{  
+		int current_sum, i;  
+		max = 0;  
+		current_sum = 0;  
+		x1 = x2 = 0;  
+		int lx1, lx2;  
+		lx1 = 0;  
+		for(int i = 0; i<n; i++)  
+		{  
+			current_sum = current_sum + input[i];  
+			if(max < current_sum)  
+			{  
+				max = current_sum;  
+				x2 = i;  
+				x1 = lx1;  
+			}  
+			if (current_sum < 0)  
+			{  
+				current_sum = 0;  
+				lx1 = i + 1;  
+			}  
+		}  
+	}
+	template <size_t rows, size_t cols>  
+	void find_max_sum(int (&inp) [rows][cols] )  
+	{  
+		int temp[cols], n, x1, x2;  
+		int current_sum, max_sum, fx1, fx2, fy1, fy2;  
+		int i,j,k;  
+		fx1 = fx2 = fy1 = fy2 = max_sum = current_sum = -1;  
+	
+		for ( i = 0; i < rows; i++ )  
+		{  
+			for( k = 0 ; k < cols; k++ )
+			{  
+				temp[k] = 0;  
+			}
+			for ( j = i ; j < rows; j++ )  
+			{  
+				for( k = 0; k < cols ; k++ ){  
+					temp[k] += input[j][k];  
+				}
+				kadane( temp, cols, x1, x2, current_sum );  
+	
+				if ( current_sum > max_sum )  
+				{  
+					fy1 = x1;  
+					fy2 = x2;  
+					fx1 = i;  
+					fx2 = j;  
+					max_sum = current_sum;  
+				}  
+			}  
+		}  
+		std::cout << "max Sum = " << max_sum << " from (" << fx1 << "," << fy1 << ") to ("  
+			<< fx2 << "," << fy2 << ")" << std::endl;  
+	}  
+
+}
+
+int main ( int argc, char ** argv )
+{
+	//        X  Y
+	int setX [3][3] =
+	{
+	  { 1, 5, 7 },
+	  { 4, 9, 2 },
+      { 2, 3, 2 }
+	};	
+
+    betacore::find_max_sum(setX); 
+    //std::cout << "Press Any Key to quit" << std::endl;
+    //std::cin.ignore( std::numeric_limits<std::streamsize>::max(), '\n' );
+    //std::cin.get();
+	return 0;
+}//main
